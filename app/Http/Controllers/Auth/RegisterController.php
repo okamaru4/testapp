06bca0acaf0074aa89f0c83b85954957f387f4af
a <>
2018-02-22 11:18:52 +0900

    Fix transition destination in login,register,logout

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/todo';
 
     /**
      * Create a new controller instance.
