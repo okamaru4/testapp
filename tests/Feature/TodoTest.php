742021ee7a6a76d853dac327b25fe0d649520f4e
a <>
2018-02-22 10:53:49 +0900

    Fix for Feature/TodoTest.php

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -25,4 +25,36 @@ class TodoTest extends TestCase
         $response = $this->get('/todo/create');
         $response->assertStatus(200);
     }
+
+    /** @test */
+    public function storeTest()
+    {
+        $this->post('todo/',
+            ['title' => "foo"]
+        );
+        $this->assertDatabaseHas('todos', ['title' => "foo"]);
+    }
+
+    /** @test */
+    public function editTest()
+    {
+        $response = $this->get('todo/1/edit');
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function updateTest()
+    {
+        $this->put('todo/1',
+                   ['title' => 'updateData']
+        );
+        $this->assertDatabaseHas('todos', ['title' => 'updateData']);
+    }
+
+    /** @test */
+    public function destroyTest()
+    {
+        $this->delete('/todo/1');
+        $this->assertDatabaseMissing('todos', ['id' => 1]);
+    }
 }
