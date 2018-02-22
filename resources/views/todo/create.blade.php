9c5e57c07bfc420f7a12c19c4dedb0baa7442117
a <>
2018-02-22 10:05:20 +0900

    install FormFacade, Fix for views

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -2,11 +2,11 @@
 @section('content')
 
     <h2 class="page-header">ToDo作成</h2>
-    <form>
+    {!! Form::open(['route' => 'todo.store']) !!}
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="ToDo内容">
+            {!! Form::input('text', 'title', null, ['required', 'class' => 'form-control', 'placeholder' => 'ToDo内容']) !!}
         </div>
         <button type="submit" class="btn btn-success pull-right">追加</button>
-    </form>
+    {!! Form::close() !!}
 
 @endsection
--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -2,11 +2,11 @@
 @section('content')
 
     <h2 class="page-header">ToDo編集</h2>
-    <form >
+    {!! Form::open(['route' => ['todo.update', $todo->id], 'method' => 'PUT']) !!}
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="ToDo内容">
+            {!! Form::input('text', 'title', $todo->title, ['required', 'class' => 'form-control']) !!}
         </div>
         <button type="submit" class="btn btn-success pull-right">更新</button>
-    </form>
+    {!! Form::close() !!}
 
 @endsection
